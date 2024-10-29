cddcd72de0a690463f5de1fba61cb67e315fb44f
HasnainTechmero <hasnaintechmero@gmail.com>
2024-10-29 16:44:48 +0530

    Initial commit

---
+++ Charades/Charades/CharadesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharadesApp.swift
+//  Charades
+//
+//  Created by Tech Baroda on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CharadesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Charades/CharadesTests/CharadesTests.swift
@@ -0,0 +1,16 @@
+//
+//  CharadesTests.swift
+//  CharadesTests
+//
+//  Created by Tech Baroda on 29/10/24.
+//
+
+import Testing
+
+struct CharadesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
